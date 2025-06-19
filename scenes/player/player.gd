7cb18d7fe7cd3bb08d8edc15e51cd93a90ec2c4d
ashdsabandal <adsabandal@up.edu.ph>
2025-06-19 15:19:53 +0800

    feat: add zero gravity effect

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,19 +1,30 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed := 200.0
 
-func _process(delta: float) -> void:
-	var direction := Vector2.ZERO
+const SPEED = 130.0
+const JUMP_VELOCITY = -400.0
 
-	if Input.is_action_pressed("strafe_right"):
-		direction.x += 1
-	if Input.is_action_pressed("strafe_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("backward"):
-		direction.y += 1
-	if Input.is_action_pressed("forward"):
-		direction.y -= 1
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
-	if direction != Vector2.ZERO:
-		direction = direction.normalized()
-		position += direction * speed * delta
+	# Handle jump.
+	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var xdirection := Input.get_axis("left", "right")
+	if xdirection:
+		velocity.x = xdirection * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 1.2)
+
+	var ydirection := Input.get_axis("up", "down")
+	if ydirection:
+		velocity.y = ydirection * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, 1.2)
+
+	move_and_slide()
