40b821f5c4a58c131bf9c85d1144681e9d215165
ashdsabandal <adsabandal@up.edu.ph>
2025-06-22 14:41:37 +0800

    feat: change player movement physics

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,29 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 120.0
-const JUMP_VELOCITY = -400.0
-	
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+#const SPEED = 120.0
+const SPEED = 5.0
+const MAX_SPEED = 135.0
 
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		#velocity.y = JUMP_VELOCITY
+
+func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var xdirection := Input.get_axis("left", "right")
 	if xdirection:
-		velocity.x = xdirection * SPEED
+		#velocity.x = xdirection * SPEED
+		velocity.x += xdirection * SPEED
+		velocity.x = clamp(velocity.x, -MAX_SPEED, MAX_SPEED)
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, 1.2)
 
 	var ydirection := Input.get_axis("up", "down")
 	if ydirection:
-		velocity.y = ydirection * SPEED
+		#velocity.y = ydirection * SPEED
+		velocity.y += ydirection * SPEED
+		velocity.y = clamp(velocity.y, -MAX_SPEED, MAX_SPEED)
 	else:
 		velocity.y = move_toward(velocity.y, 0, 1.2)
 
